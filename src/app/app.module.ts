04406147c7a510f7a481925f89c8fddebcf70cce
JCYhapon <98503854+JCYhapon@users.noreply.github.com>
2023-03-21 18:00:41 +0800

    4 files modified, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,9 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
+
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
-
 
 @NgModule({
   imports: [
@@ -20,26 +19,22 @@ import { ShippingComponent } from './shipping/shipping.component';
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
       { path: 'shipping', component: ShippingComponent },
-    ])
+    ]),
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent
-  ],
-  bootstrap: [
-    AppComponent
-  ]
-})
-export class AppModule { }
 
+    CartComponent,
+    ShippingComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
 
 /*
 Copyright Google LLC. All Rights Reserved.
